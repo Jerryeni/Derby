98146e1e553f58cd6627042c24e1f0cf840607ac
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2025-01-22 18:13:17 +0100

    Update provider.tsx

--- providers/provider.tsx
+++ providers/provider.tsx
@@ -1,50 +1,62 @@
 "use client";
 
-import React, { createContext, useContext, ReactNode, useEffect } from 'react';
-import { usePresale as usePresaleHook } from '@/hooks/usePresale';
+import React, { createContext, useContext, ReactNode, useEffect } from "react";
+import { usePresale as usePresaleHook } from "@/hooks/usePresale";
 
-const PresaleContext = createContext<ReturnType<typeof usePresaleHook> | null>(null);
+const PresaleContext = createContext<ReturnType<typeof usePresaleHook> | null>(
+  null
+);
 
 export const PresaleProvider = ({ children }: { children: ReactNode }) => {
-  const {status,
+  const {
+    status,
     uccInfo,
     userUCCInfo,
     userAddress,
-    totalTokens,curPage,
+    totalTokens,
+    curPage,
     claimVirtualTokens,
     setCurPage,
     buyWithUSDT,
     buyWithBNB,
     resetStatus,
-    initWallet} = usePresaleHook();
+    initWallet,
+    getLevelDetails,
+  } = usePresaleHook();
 
-useEffect(() => {
-  initWallet().then(() => {
-    console.log("done")
-  });
-},[curPage,userAddress,totalTokens])
+  useEffect(() => {
+    initWallet().then(() => {
+      console.log("done");
+    });
+  }, [curPage, userAddress, totalTokens]);
 
   return (
-    <PresaleContext.Provider value={{status,
+    <PresaleContext.Provider
+      value={{
+        status,
         uccInfo,
         userUCCInfo,
         userAddress,
-        totalTokens,curPage,
+        totalTokens,
+        curPage,
         setCurPage,
         claimVirtualTokens,
         buyWithUSDT,
         buyWithBNB,
         resetStatus,
-        initWallet}}>
+        initWallet,
+        getLevelDetails,
+      }}
+    >
       {children}
-    </PresaleContext.Provider>    
+    </PresaleContext.Provider>
   );
 };
 
 export const usePresale = () => {
   const context = useContext(PresaleContext);
   if (!context) {
-    throw new Error('usePresale must be used within a PresaleProvider');
+    throw new Error("usePresale must be used within a PresaleProvider");
   }
   return context;
 };
